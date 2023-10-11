6ac9dea360274245fd7be7fc30c13468b37e3585
AB <35473718+arsalanbardsiri@users.noreply.github.com>
2023-10-10 19:41:01 -0700

    Refactor server.js: Add Handlebars setup, path utility for static files, and enhance error handling.

--- server.js
+++ server.js
@@ -1,39 +1,40 @@
-// Import necessary modules
+// Import required modules
 const express = require('express');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
+const path = require('path');
 require('dotenv').config(); // Load environment variables from .env file
 
-const app = express(); // Create an instance of the Express app
-const PORT = process.env.PORT || 3000; // Define the port
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 // Middleware setup
-app.use(express.json());  // Parse JSON request body
-app.use(express.urlencoded({ extended: true })); // Parse URL-encoded request body
-app.use(express.static('./public')); // Serve static files from the "public" directory
+app.use(express.json());  // Parse incoming JSON request bodies
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded request bodies
+app.use(express.static(path.join(__dirname, 'public'))); // Serve static assets
+
+// Set up Handlebars for templating
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 // Session configuration
 const sess = {
-  secret: process.env.SESSION_SECRET,  // Secret key from .env
-  store: new SequelizeStore({          // Use Sequelize to store session data
-    db: sequelize,
-  }),
+  secret: process.env.SESSION_SECRET,
+  store: new SequelizeStore({ db: sequelize }),
   resave: false,
   saveUninitialized: true,
-  cookie: {
-    httpOnly: true,                    // Prevent client-side scripts from accessing the cookie
-    // secure: true,                  // Uncomment this line when deploying over HTTPS
-  }
+  cookie: { httpOnly: true }
 };
 
-app.use(session(sess)); // Apply the session middleware
+app.use(session(sess));
 
-// Route handling
-app.use('/api', require('./routes/apiRoutes')); // API routes
-app.use('/', require('./routes/htmlRoutes'));   // View routes
+// Define routes
+app.use('/api', require('./routes/apiRoutes'));
+app.use('/', require('./routes/htmlRoutes'));
 
-// Catch 404 and forward to error handler
+// 404 error handling
 app.use((req, res, next) => {
     const err = new Error('Not Found');
     err.status = 404;
@@ -44,13 +45,11 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page (assumes you have an error.handlebars or similar view)
     res.status(err.status || 500);
     res.render('error');
 });
 
-// Sync Sequelize models, then start the server
+// Start server after syncing with the database
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
 });
