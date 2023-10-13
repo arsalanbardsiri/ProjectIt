beb38c666f8673746302c8a4e97ad02fdd036312
AB <35473718+arsalanbardsiri@users.noreply.github.com>
2023-10-12 19:25:41 -0700

    initial middleware modification

---
+++ config/middleware.js
@@ -0,0 +1,9 @@
+// config/middleware.js
+const errorHandler = (err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+};
+
+module.exports = {
+    errorHandler
+};
