f3ec7e8601a22a84e9b96cea0511c320ca08101d
AB <35473718+arsalanbardsiri@users.noreply.github.com>
2023-10-13 22:31:37 -0700

    require('dotenv').config(); was needed added back

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,5 @@
 const Sequelize = require('sequelize');
+require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
@@ -7,7 +8,7 @@ const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME
         max: 5,
         min: 0,
         idle: 10000
-    },
+    }
 });
 
 module.exports = sequelize;
