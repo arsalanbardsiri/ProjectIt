23eaa6d255bca90357f662e27d17d9ca4b721a27
AB <35473718+arsalanbardsiri@users.noreply.github.com>
2023-10-13 23:28:52 -0700

    revised server.js

--- server.js
+++ server.js
@@ -1,46 +1,47 @@
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const passport = require('./config/passport');  // Adding passport for authentication
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const passport = require('./config/passport');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Set up sessions
+// Middleware for body parsing
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Static directory
+app.use(express.static("public"));
+
+// Set up Handlebars as the default templating engine.
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// Set up sessions with Sequelize
 const sess = {
     secret: process.env.SESSION_SECRET,
     store: new SequelizeStore({
         db: sequelize
     }),
     resave: false,
-    saveUninitialized: false,
+    saveUninitialized: true,
     cookie: {}
 };
-
 app.use(session(sess));
 
-// Use handlebars as the default template engine
-app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
-
-// Set up body parsing middlewares
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// Serve static files from the "public" directory
-app.use(express.static('public'));
-
-// Initialize passport and session
+// Initialize passport and the express session and tell Passport to use session to keep our user logged in
 app.use(passport.initialize());
 app.use(passport.session());
 
-// Use the routes
-require('./routes/apiRoutes')(app);  // Prefixing with /api for clarity
-require('./routes/htmlRoutes')(app);
+// Routes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
-// Start the server after syncing the database models
+// Sync the database and then start the server
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => {
         console.log('App listening on PORT ' + PORT);
