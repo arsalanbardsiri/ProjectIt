e1dee0f1fe24da3afede98c73494e4527e6e3e7c
AB <35473718+arsalanbardsiri@users.noreply.github.com>
2023-10-13 22:23:48 -0700

    printing out errors for debuggin apiroute the /users/register

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -27,7 +27,8 @@ router.post('/users/register', async (req, res) => {
         res.status(201).json({ message: "Registration successful!" });
 
     } catch (error) {
-        res.status(500).json({ message: "Error registering user.", error });
+        console.error("Error during registration:", error);
+        res.status(500).json({ message: "Error registering user.", error: error.message });
     }
 });
 
