caefde9050ffd6c08ca4a1a298e811d956bd1184
AB <35473718+arsalanbardsiri@users.noreply.github.com>
2023-10-13 20:23:17 -0700

    Models setup, index simpler, studyRoom and User to bgin with usr pss emil.

---
+++ models/studyRoom.js
@@ -0,0 +1,39 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class StudyRoom extends Model {}
+
+StudyRoom.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: true
+        },
+        userId: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'studyroom'
+    }
+);
+
+module.exports = StudyRoom;
