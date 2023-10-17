83ae768cdcdaa51998687c28e83060967622400b
AB <35473718+arsalanbardsiri@users.noreply.github.com>
2023-10-16 18:28:30 -0700

    Added JAWSDB_URL to connection for prodcution, heroku, debugging.

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,20 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-    pool: {
-        max: 5,
-        min: 0,
-        idle: 10000
-    }
-});
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+        host: process.env.DB_HOST,
+        dialect: 'mysql',
+        pool: {
+            max: 5,
+            min: 0,
+            idle: 10000
+        }
+    });
+}
 
 module.exports = sequelize;
