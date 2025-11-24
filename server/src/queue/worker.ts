6fe91d0dd4830d87e2d0287a2c7305b5ae2f8f2e
AB <35473718+arsalanbardsiri@users.noreply.github.com>
2025-11-24 10:39:49 -0800

    ix: start worker in main process and auto-join room on visit

--- server/src/queue/worker.ts
+++ server/src/queue/worker.ts
@@ -27,7 +27,9 @@ const processJobs = async () => {
 
                 if (jobData.type === 'WELCOME_EMAIL') {
                     const transporter = nodemailer.createTransport({
-                        service: 'gmail',
+                        host: 'smtp.gmail.com',
+                        port: 465,
+                        secure: true, // Use SSL
                         auth: {
                             user: process.env.EMAIL_USER,
                             pass: process.env.EMAIL_PASS,
